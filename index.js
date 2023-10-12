d63586e42e76a50e18de7b3d5b6bc2a64e0db609
Cdric2Leroy <cedric.leroy.psycho@gmail.com>
2023-10-12 21:25:31 +0100

    fix(index.js): add multiple correction to the initial script

--- index.js
+++ index.js
@@ -6,55 +6,57 @@ const userRoutes = require('./routes/userRoutes');
 const app = express();
 var db;
 
-function createTables(db) {
-    db.exec(`
-    create table user (
-        id int primary key not null,
-        last_name text not null,
-        first_name text not null,
-        ip_addr text not null,
-        city text not null
-    );
-        `, ()  => {
-        runQueries(newdb);
-
-        //
-        const arrUsers = require('./data/mock.json')
-        for (let user of arrUsers) {
-            runQueries(`
-                insert into user (first_name, last_name, city)
-                values ('${user['first_name']}', '${user['last_name']}', '${user['city']}');
-               `)
-        }
-    });
-}
-
 function createDatabase() {
     db = new sqlite3.Database('iwanttoworkatdarwinx.db', (err) => {
         if (err) {
             console.log("Getting error " + err);
-            exit(1);
+            process.exit(1);
         }
-        createTables(newdb);
+        createTables(db);
     });
 }
 
+function createTables(db) {
+    db.exec(`
+    CREATE TABLE IF NOT EXISTS user (
+        id INTEGER PRIMARY KEY NOT NULL,
+        last_name TEXT NOT NULL,
+        first_name TEXT NOT NULL,
+        ip_addr TEXT NOT NULL,
+        city TEXT NOT NULL
+    );
+        `, ()  => {
+        runQueries(db);
+
+        //
+        const arrUsers = require('./data/mock.json')
+        for (let user of arrUsers) {
+            db.run(`
+                INSERT INTO user (first_name, last_name, city)
+                VALUES ('${user['id']}', '${user['first_name']}', '${user['last_name']}', '${user['ip_address']}', '${user['city']}');
+               `);
+        }
+    });
+}
 new sqlite3.Database('./iwanttoworkatdarwinx.db', sqlite3.OPEN_READWRITE, (err) => {
-    if (err && err.code == "SQLITE_CANTOPEN") {
+    if (err && err.code === "SQLITE_CANTOPEN") {
         createDatabase();
         return;
     } else if (err) {
         console.log("Getting error " + err);
-        exit(1);
+        process.exit(1);
     }
 });
 
 
+
+
+
 app.get('/', function (req, res) {
     res.send('DarwinX - Job Tech Test');
-})
+});
 
-app.use('/users', userRoutes)
+app.use('/users', userRoutes);
 
 const server = app.listen(8081, function () {
 
